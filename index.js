f24e90dc71debe4d52000d32650e17488ba04b72
David Feng <david.feng@edu.uwaterloo.ca>
2019-03-17 18:34:28 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+var _ = require('underscore');
+
+// Core module
+// File or folder (inside folder would be an index.js file)
+// node_modules
+
+var result = _.contains([1, 2, 3], 2)
+ 
+console.log(result);
